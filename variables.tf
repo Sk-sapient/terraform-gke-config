3157409f83712851aa4e85f4bb8bb23ab16ebc04
Sk-sapient <shatrudhnkrsingh95@gmail.com>
2024-06-11 20:46:27 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "region" {
+  description = "Region to deploy resources"
+  type        = string
+}
+
+variable "zone" {
+  description = "Zone to deploy resources"
+  type        = string
+}
+
+variable "project" {
+  description = "GCP project"
+  type        = string
+}
+
+variable "network" {
+  description = "VPC network name"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "GKE cluster name"
+  type        = string
+}
+
+variable "node_count" {
+  description = "Number of nodes in the node pool"
+  type        = number
+}
+
+variable "machine_type" {
+  description = "Machine type for nodes"
+  type        = string
+}
+
+variable "preemptibility" {
+  description = "Whether to use preemptible VMs"
+  type        = bool
+}
