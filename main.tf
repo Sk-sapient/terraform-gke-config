0801dbb7c64e61e90090e0b4dd13e0cfa0a71b3a
Sk-sapient <shatrudhnkrsingh95@gmail.com>
2024-06-11 23:22:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,58 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "burner-tf-state"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.10.0"
+    }
+  }
+}
+
 module "vpc" {
   source = "./vpc"
+  project = var.project
+  network = var.network
 }
 
 module "firewall" {
   source = "./firewall"
+  network = module.vpc.vpc_network
 }
 
 module "router" {
   source = "./router"
+  network = module.vpc.vpc_network
 }
 
 module "gke" {
-  source       = "./gke"
-  region       = var.region
-  zone         = var.zone
-  cluster_name = var.cluster_name
-}
-
-module "node_pool" {
-  source         = "./node_pool"
-  zone           = var.zone
-  node_count     = var.node_count
-  machine_type   = var.machine_type
+  source        = "./gke"
+  cluster_name  = var.cluster_name
+  region        = var.region
+  zone          = var.zone
+  machine_type  = var.machine_type
+  node_count    = var.node_count
   preemptibility = var.preemptibility
+  project       = var.project
+  network       = module.vpc.vpc_network.self_link
+  subnetwork    = module.vpc.private_subnetwork.self_link
 }
 
-module "ingress" {
-  source = "./ingress"
-  region = var.region
+output "cluster_name" {
+  value = module.gke.cluster_name
 }
 
+output "kubernetes_endpoint" {
+  value = module.gke.kubernetes_endpoint
+}
+
+output "kubernetes_ca_certificate" {
+  value = module.gke.kubernetes_ca_certificate
+}
