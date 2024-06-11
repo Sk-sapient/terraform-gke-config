9f804c2055cfce56a5ffbd3ba3e7aca597126a2c
Sk-sapient <shatrudhnkrsingh95@gmail.com>
2024-06-11 21:29:22 +0530

    Update main.tf

--- ingress/main.tf
+++ ingress/main.tf
@@ -1,57 +1,58 @@
-resource "google_compute_address" "static_ip" {
-  name         = "ingress-static-ip"
-  region       = var.region
-  address_type = "EXTERNAL"
+resource "google_compute_region_backend_service" "backend_service" {
+  name = "backend-service"
+
+  timeout_sec          = 30
+  protocol             = "HTTP"
+  port_name            = "http"
+  enable_cdn           = false
+  session_affinity     = "NONE"
+  affinity_cookie_ttl_sec = 0
+
+  health_checks = [google_compute_region_health_check.http_health_check.id]
 }
 
-resource "kubernetes_service" "nginx_ingress" {
-  metadata {
-    name      = "nginx-ingress-controller"
-    namespace = "kube-system"
-    annotations = {
-      "cloud.google.com/load-balancer-type" = "External"
-    }
-  }
+resource "google_compute_url_map" "url_map" {
+  name            = "url-map"
+  default_service = google_compute_region_backend_service.backend_service.self_link
+}
 
-  spec {
-    type = "LoadBalancer"
-    load_balancer_ip = google_compute_address.static_ip.address
-    ports {
-      port        = 80
-      target_port = 80
-    }
-    ports {
-      port        = 443
-      target_port = 443
-    }
-    selector = {
-      app = "nginx-ingress"
-    }
+resource "google_compute_backend_service" "internal_lb" {
+  name = "internal-lb"
+  port_name = "http"
+  protocol = "HTTP"
+  timeout_sec = 30
+
+  health_checks = [google_compute_health_check.http_health_check.id]
+
+  backend {
+    group = google_container_node_pool.primary.node_pool_id
   }
 }
 
-resource "kubernetes_deployment" "nginx_ingress" {
-  metadata {
-    name      = "nginx-ingress-controller"
-    namespace = "kube-system"
+resource "google_compute_global_forwarding_rule" "forwarding_rule" {
+  name       = "forwarding-rule"
+  target     = google_compute_backend_service.internal_lb.self_link
+  port_range = "80"
+  load_balancing_scheme = "INTERNAL"
+}
+
+resource "google_compute_health_check" "http_health_check" {
+  name               = "http-health-check"
+  check_interval_sec = 10
+  timeout_sec        = 5
+  tcp_health_check {
+    port = 80
+  }
+}
+
+resource "google_compute_firewall" "internal_lb_firewall" {
+  name    = "internal-lb-firewall"
+  network = module.vpc.vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
   }
 
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        app = "nginx-ingress"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "nginx-ingress"
-        }
-      }
-
-      spec {
-        container {
-          name  = "nginx-ingress-controller"
-          image = "quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.
+  source_ranges = ["10.0.0.0/8"] # Adjust this range to match your VPC CIDR range
+}
