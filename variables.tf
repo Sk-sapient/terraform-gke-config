65bfd611d0e9ac99baba85e7bc26dd1964f94e23
Sk-sapient <shatrudhnkrsingh95@gmail.com>
2024-06-12 04:16:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,40 +1,48 @@
 variable "project" {
   description = "GCP project"
   type        = string
+  default     = "burner-kumshatr"
 }
 
 variable "region" {
   description = "Region to deploy resources"
   type        = string
+  default     = "us-central1"
 }
 
 variable "zone" {
   description = "Zone to deploy resources"
   type        = string
+  default     = "us-central1-a"
 }
 
 variable "cluster_name" {
   description = "GKE cluster name"
   type        = string
+  default     = "private-gke-cluster"
 }
 
 variable "vpc_network_id" {
   description = "VPC network name"
   type        = string
+  default     = vpc_network
 }
 
 variable "subnetwork" {
   description = "subnetwork network name"
   type        = string
+  default     = gke-private-subnet
 }
 variable "node_count" {
   description = "Number of nodes in the node pool"
   type        = number
+  default     = 1
 }
 
 variable "machine_type" {
   description = "Machine type for nodes"
   type        = string
+  default     = e2-small
 }
 
 variable "preemptibility" {
