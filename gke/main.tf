94e8bc8be39f8f9cc68a94a92c98110332de84d2
Sk-sapient <shatrudhnkrsingh95@gmail.com>
2024-06-11 21:20:56 +0530

    Update main.tf

--- gke/main.tf
+++ gke/main.tf
@@ -1,52 +1,21 @@
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.zone
-
+  name                   = var.cluster_name
+  location               = var.zone
   remove_default_node_pool = true
-  initial_node_count       = 1
-
-  network    = module.vpc.vpc_network.self_link
-  subnetwork = module.vpc.private.self_link
+  initial_node_count     = 1
+  network                = module.vpc.vpc_network.self_link
+  subnetwork             = module.vpc.private.self_link
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = true
+  }
 
   logging_service    = "logging.googleapis.com/kubernetes"
   monitoring_service = "monitoring.googleapis.com/kubernetes"
 
   networking_mode = "VPC_NATIVE"
 
-  deletion_protection = true
-
-  node_locations = [
-    var.zone
-  ]
-
-  addons_config {
-    http_load_balancing {
-      disabled = false
-    }
-
-    horizontal_pod_autoscaling {
-      disabled = false
-    }
-  }
-
-  release_channel {
-    channel = "REGULAR"
-  }
-
-  workload_identity_config {
-    workload_pool = "${var.project}.svc.id.goog"
-  }
-
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "k8s-pod-range"
-    services_secondary_range_name = "k8s-service-range"
-  }
-
-  private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"
-  }
+  # Other configurations...
 }
 
 output "cluster_name" {
@@ -54,7 +23,7 @@ output "cluster_name" {
 }
 
 output "kubernetes_endpoint" {
-  value = google_container_cluster.primary.endpoint
+  value = google_container_cluster.primary.private_endpoint
 }
 
 output "kubernetes_ca_certificate" {
